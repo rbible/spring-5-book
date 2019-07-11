0f889b1d345646edfd38e2a01104e793100b5aba
renbing <beck.ren@qq.com>
2019-07-11 12:14:16 +0800

    format and logs
    format and logs

--- samples/s5-ch02-custom-scope-annotation/src/main/java/com/waylau/spring/scope/Application.java
+++ samples/s5-ch02-custom-scope-annotation/src/main/java/com/waylau/spring/scope/Application.java
@@ -13,6 +13,7 @@ import com.waylau.spring.scope.service.MessageService;
 
 /**
  * Application Main.
+ * bean scope: 线程
  *
  * @author <a href="https://waylau.com">Way Lau</a>
  * @since 1.0.0 2019年2月13日
@@ -21,8 +22,7 @@ public class Application {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         @SuppressWarnings("resource")
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 		/*
 		MessageService messageService = context.getBean(MessageService.class);
 		messageService.getMessage();
@@ -31,32 +31,25 @@ public class Application {
 		messageService2.getMessage();
 		*/
 
-        CompletableFuture<String> task1 = CompletableFuture.supplyAsync(() -> {
-            //模拟执行耗时任务
-            MessageService messageService = context.getBean(MessageService.class);
-            messageService.getMessage();
+        CompletableFuture<String> task1 = getAndRunService(context);
 
-            MessageService messageService2 = context.getBean(MessageService.class);
-            messageService2.getMessage();
-            //返回结果
-            return "result";
-        });
+        CompletableFuture<String> task2 = getAndRunService(context);
 
-        CompletableFuture<String> task2 = CompletableFuture.supplyAsync(() -> {
-            //模拟执行耗时任务
-            MessageService messageService = context.getBean(MessageService.class);
-            messageService.getMessage();
-
-            MessageService messageService2 = context.getBean(MessageService.class);
-            messageService2.getMessage();
-            //返回结果
-            return "result";
-        });
-		
         task1.get();
         task2.get();
+    }
 
+    private static CompletableFuture<String> getAndRunService(ApplicationContext context) {
+        return CompletableFuture.supplyAsync(() -> {
+            //模拟执行耗时任务
+            MessageService messageService = context.getBean(MessageService.class);
+            messageService.getMessage();
 
+            MessageService messageService2 = context.getBean(MessageService.class);
+            messageService2.getMessage();
+            //返回结果
+            return "result";
+        });
     }
 
 }
