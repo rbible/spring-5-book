1a114f6432d8c968d6be640fcd842dc96f45589e
renbing <beck.ren@qq.com>
2019-08-19 21:26:38 +0800

    java 原生，xml 转 object

--- samples/s5-ch10-jaxb-oxm/src/main/java/com/waylau/spring/jaxb/service/CityDataServiceImpl.java
+++ samples/s5-ch10-jaxb-oxm/src/main/java/com/waylau/spring/jaxb/service/CityDataServiceImpl.java
@@ -14,9 +14,9 @@ import com.waylau.spring.jaxb.vo.CityList;
 
 /**
  * City Data Service.
- * 
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
  * @since 1.0.0 2018年3月9日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 public class CityDataServiceImpl implements CityDataService {
 
@@ -25,16 +25,16 @@ public class CityDataServiceImpl implements CityDataService {
 		Resource resource = new ClassPathResource("citylist.xml");
 		BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream(), "utf-8"));
 		StringBuffer buffer = new StringBuffer();
-		String line = "";
-		
-		while ((line = br.readLine()) !=null) {
+		String line;
+
+		while ((line = br.readLine()) != null) {
 			buffer.append(line);
 		}
-		
+
 		br.close();
-		
+
 		// XML转为Java对象
-		CityList cityList = (CityList)XmlBuilder.xmlStrToOject(CityList.class, buffer.toString());
+		CityList cityList = (CityList) XmlBuilder.xmlStrToOject(CityList.class, buffer.toString());
 		return cityList.getCityList();
 	}
 
--- samples/s5-ch10-jaxb-oxm/src/main/java/com/waylau/spring/jaxb/util/XmlBuilder.java
+++ samples/s5-ch10-jaxb-oxm/src/main/java/com/waylau/spring/jaxb/util/XmlBuilder.java
@@ -1,7 +1,5 @@
 package com.waylau.spring.jaxb.util;
 
-
-
 import java.io.Reader;
 import java.io.StringReader;
 
@@ -10,9 +8,9 @@ import javax.xml.bind.Unmarshaller;
 
 /**
  * Xml Builder.
- * 
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
  * @since 1.0.0 2017年11月23日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 public class XmlBuilder {
 
@@ -23,21 +21,25 @@ public class XmlBuilder {
 	 * @return
 	 * @throws Exception
 	 */
+
+	/**
+	 * mynote： xml 转 object
+	 */
 	public static Object xmlStrToOject(Class<?> clazz, String xmlStr) throws Exception {
-		Object xmlObject = null;
-		Reader reader = null;
+		Object xmlObject;
+		Reader reader;
 		JAXBContext context = JAXBContext.newInstance(clazz);
-		
+
 		// XML 转为对象的接口
 		Unmarshaller unmarshaller = context.createUnmarshaller();
-		
+
 		reader = new StringReader(xmlStr);
 		xmlObject = unmarshaller.unmarshal(reader);
-		
+
 		if (null != reader) {
 			reader.close();
 		}
-		
+
 		return xmlObject;
 	}
 }
