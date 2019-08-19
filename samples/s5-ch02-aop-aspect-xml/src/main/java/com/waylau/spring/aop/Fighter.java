834378f3644017bb19912d08378eae8499460134
renbing <beck.ren@qq.com>
2019-08-19 17:50:15 +0800

    xml 方式配置 aop：before、after、around

--- samples/s5-ch02-aop-aspect-xml/src/main/java/com/waylau/spring/aop/Fighter.java
+++ samples/s5-ch02-aop-aspect-xml/src/main/java/com/waylau/spring/aop/Fighter.java
@@ -3,20 +3,32 @@
  */
 package com.waylau.spring.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 /**
  * Fighter.
- * 
- * @since 1.0.0 2018年2月1日
+ *
  * @author <a href="https://waylau.com">Way Lau</a>
+ * @since 1.0.0 2018年2月1日
  */
 public class Fighter {
 
-	public void foundBefore() {
-		System.out.println("Fighter wait for tiger...");
-	}
+    public void foundBefore() {
+        System.out.println("Before: Fighter wait for tiger...");
+    }
 
-	public void foundAfter() {
-		System.out.println("Fighter fight with tiger...");
-	}
+    public void foundAfterReturn() {
+        System.out.println("AfterReturn: Fighter fight with tiger...");
+    }
+
+    public void foundAround(ProceedingJoinPoint point) throws Throwable {
+        System.out.println("AroundBefore: Fighter wait for tiger...");
+        point.proceed();
+        System.out.println("AroundAfter: Fighter wait for tiger...");
+    }
+
+    public void foundAfter() {
+        System.out.println("After: Fighter after for tiger...");
+    }
 
 }
