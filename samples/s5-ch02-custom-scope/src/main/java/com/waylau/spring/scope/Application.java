0a20ec5379509b5f5c09a8457477220b4b826f06
renbing <beck.ren@qq.com>
2019-08-19 18:08:05 +0800

    getBean使用：可用 ID、class、id&class

--- samples/s5-ch02-custom-scope/src/main/java/com/waylau/spring/scope/Application.java
+++ samples/s5-ch02-custom-scope/src/main/java/com/waylau/spring/scope/Application.java
@@ -17,8 +17,14 @@ public class Application {
     public static void main(String[] args) {
         @SuppressWarnings("resource")
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        MessagePrinter printer = context.getBean("messagePrinter", MessagePrinter.class);
         printer.printMessage();
+
+        /**
+         * mynote: getBean 使用，可用 id、class
+         */
+        MessagePrinter messagePrinter2 = context.getBean("messagePrinter2", MessagePrinter.class);
+        messagePrinter2.printMessage();
     }
 
 }
