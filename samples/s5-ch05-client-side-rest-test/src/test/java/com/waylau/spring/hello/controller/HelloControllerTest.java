8ae66dea4d27209ec401c18e86e635e83d4cc91a
renbing <beck.ren@qq.com>
2019-08-19 20:28:42 +0800

    web request mock

--- samples/s5-ch05-client-side-rest-test/src/test/java/com/waylau/spring/hello/controller/HelloControllerTest.java
+++ samples/s5-ch05-client-side-rest-test/src/test/java/com/waylau/spring/hello/controller/HelloControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
@@ -39,17 +40,15 @@ public class HelloControllerTest {
 
 	@Test
 	public void performGet() throws Exception {
-
 		String responseBody = "Hello World! Welcome to visit waylau.com!";
-
 		this.mockServer.expect(requestTo("/hello")).andExpect(method(HttpMethod.GET))
 			.andRespond(withSuccess(responseBody, MediaType.APPLICATION_JSON));
 
 		@SuppressWarnings("unused")
 		String hello = this.restTemplate.getForObject("/hello", String.class);
-
+		System.out.println("response: " + hello);
+		
 		// 我们只验证请求。响应是被 Mock 掉的
-
 		this.mockServer.verify();
 	}
 }
