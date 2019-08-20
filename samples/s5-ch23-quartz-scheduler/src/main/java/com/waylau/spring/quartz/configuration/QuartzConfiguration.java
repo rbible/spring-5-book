35ef7332a10954b6b739875b6555c539517dbea8
renbing <beck.ren@qq.com>
2019-08-20 23:59:36 +0800

    quartz 定时作业的几个 bean 定义：jobDetail、trigger、scheduler

--- samples/s5-ch23-quartz-scheduler/src/main/java/com/waylau/spring/quartz/configuration/QuartzConfiguration.java
+++ samples/s5-ch23-quartz-scheduler/src/main/java/com/waylau/spring/quartz/configuration/QuartzConfiguration.java
@@ -3,6 +3,8 @@
  */
 package com.waylau.spring.quartz.configuration;
 
+import org.quartz.JobDetail;
+import org.quartz.SimpleTrigger;
 import org.quartz.spi.JobFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,38 +16,44 @@ import com.waylau.spring.quartz.job.WeatherDataSyncJob;
 
 /**
  * Quartz 配置类.
- * 
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
  * @since 1.0.0 2017年10月23日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 @Configuration
 public class QuartzConfiguration {
-	
+
 	@Bean
-	public JobDetailFactoryBean jobDetailFactoryBean(){
+	public JobDetailFactoryBean jobDetailFactoryBean() {
 		JobDetailFactoryBean factory = new JobDetailFactoryBean();
 		factory.setJobClass(WeatherDataSyncJob.class);
 		return factory;
 	}
-	
+
 	@Bean
-	public SimpleTriggerFactoryBean simpleTriggerFactoryBean(){
+	public SimpleTriggerFactoryBean simpleTriggerFactoryBean() {
+		JobDetail jobDetail = jobDetailFactoryBean().getObject();
+
 		SimpleTriggerFactoryBean stFactory = new SimpleTriggerFactoryBean();
-		stFactory.setJobDetail(jobDetailFactoryBean().getObject());
+		stFactory.setJobDetail(jobDetail);
 		stFactory.setStartDelay(3000);  // 延迟3秒
 		stFactory.setRepeatInterval(30000); // 间隔30秒
 		return stFactory;
 	}
+
 	@Bean
 	public JobFactory jobFactory() {
-	    return new QuartzJobFactory();
+		return new QuartzJobFactory();
 	}
-	
+
 	@Bean
 	public SchedulerFactoryBean schedulerFactoryBean() {
+		SimpleTrigger trigger = simpleTriggerFactoryBean().getObject();
+		JobFactory jobFactory = jobFactory();
+
 		SchedulerFactoryBean scheduler = new SchedulerFactoryBean();
-		scheduler.setTriggers(simpleTriggerFactoryBean().getObject());
-		scheduler.setJobFactory(jobFactory());
+		scheduler.setTriggers(trigger);
+		scheduler.setJobFactory(jobFactory);
 		return scheduler;
 	}
 
--- samples/s5-ch23-quartz-scheduler/src/main/java/com/waylau/spring/quartz/job/WeatherDataSyncJob.java
+++ samples/s5-ch23-quartz-scheduler/src/main/java/com/waylau/spring/quartz/job/WeatherDataSyncJob.java
@@ -14,28 +14,23 @@ import com.waylau.spring.quartz.service.WeatherDataService;
 
 /**
  * 天气数据同步任务.
- * 
+ *
+ * @author <a href="https://waylau.com">Way Lau</a>
  * @since 1.0.0 2018年4月18日
- * @author <a href="https://waylau.com">Way Lau</a> 
  */
 public class WeatherDataSyncJob extends QuartzJobBean {
-	
-	private final static Logger logger = LoggerFactory.getLogger(WeatherDataSyncJob.class); 
-	
+
+	private final static Logger logger = LoggerFactory.getLogger(WeatherDataSyncJob.class);
+
 	@Autowired
 	private WeatherDataService weatherDataService;
-	
+
 	@Override
 	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-		logger.info("Start 天气数据同步任务");
-		
- 		String cityId = "101280301"; // 惠州
-
- 		logger.info("天气数据同步任务中，cityId:" + cityId);
-		
+		logger.info("\n ************  Start 天气数据同步任务");
+		String cityId = "101280301"; // 惠州
+		logger.info("\n ************ 天气数据同步任务中，cityId:" + cityId);
 		// 根据城市ID获取天气
- 		logger.info(weatherDataService.getDataByCityId(cityId).getData().toString());
- 		
+		logger.info("\n ************ " + weatherDataService.getDataByCityId(cityId).getData().toString());
 	}
-
 }
